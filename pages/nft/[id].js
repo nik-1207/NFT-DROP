2c585f977eca2165fa1cf1bd310a9de718113656
nik-1207 <nmknikhilkashyap@gmail.com>
2022-08-18 14:50:54 +0530

    update yarn lock file

--- pages/nft/[id].js
+++ pages/nft/[id].js
@@ -8,7 +8,6 @@ const NFTDropPage = () => {
 
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
-      {/* left */}
       <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
           <div className="bg-gradient-to-br from-yellow-400 to-purple-600 p-2 rounded-xl">
@@ -22,7 +21,7 @@ const NFTDropPage = () => {
           <div className="space-y-2 p-5 text-center">
             <h1 className="text-4xl font-bold text-white">PAPAFAM Apes</h1>
             <h2 className="text-xl text-gray-300">
-              A collection of PAPAFAM Apes who live and breathe React!
+              A demo collection of Apes who live and breathe React!
             </h2>
           </div>
         </div>
@@ -33,7 +32,7 @@ const NFTDropPage = () => {
           <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
             The{" "}
             <span className="font-extrabold underline decoration-pink-600/50">
-              PAPAFAM
+              Nik-1207
             </span>{" "}
             NFT Market place
           </h1>
@@ -58,7 +57,7 @@ const NFTDropPage = () => {
             alt="apes collection"
           />
           <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
-            The PAPAFAM Ape Coding Club | NFT Drop
+            The Nik-1207 NFT Drop website
           </h1>
           <p className="pt-2text-xl text-green-500">13 /21 Nft's claimed</p>
         </div>
